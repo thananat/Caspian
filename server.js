3109a748eb05b051a8dbd298fbf7b9af5fd8c677
Ubuntu <ubuntu@ip-172-31-8-40.ap-southeast-1.compute.internal>
2013-06-28 05:02:57 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+console.log("Initialized server at port 3000");
+app.listen(3000);
+
+
+app.use(express.bodyParser());
+
+app.post('/endpoint', function(req, res){
+    console.log("Data received");
+	console.log("Host: " + req.host);
+    console.log('Body: ' + JSON.stringify(req.body));
+
+	var data = {response:'json'};
+	console.log("Responding with: " + JSON.stringify(data));
+
+	res.send(data);
+});
+
