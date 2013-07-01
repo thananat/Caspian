24f564fc5bd761c6934c3c845871f375918b7837
Thananat Jitapunkul <thananat@gmail.com>
2013-07-01 00:29:06 +0000

    Updated the homepage and server to workable copy

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 var express = require('express');
 var app = express();
 
-app.use(express.bodyParser());
-
-app.get('/', function(req, res){
-    res.send("Hello world!");
-});
-
 console.log("Initialized server at port 3000");
 app.listen(3000);
 
 
-/**
+app.use(express.bodyParser());
+app.use(express.logger('dev'));
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res) {
+	res.send("Hello world!");
+});
+
+app.get('/endpoint', function(req, res) {
+	console.log("GET Received by server");
+	res.contentType('text/plain');
+	res.send("GET received...");
+});
+
 app.post('/endpoint', function(req, res){
     console.log("Data received");
 	console.log("Host: " + req.host);
     console.log('Body: ' + JSON.stringify(req.body));
 
-	var data = {response:'json'};
-	console.log("Responding with: " + JSON.stringify(data));
+	console.log("Responding with: " + JSON.stringify(req.body));
+	res.json(req.body);
 
-	res.send(data);
+//	res.send({name: 'Thananat Jitapunkul', age: '25'});
 });
-**/
 
