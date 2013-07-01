70607c2c782520bae0007c2f370e258a05cbeb58
Thananat Jitapunkul <thananat@gmail.com>
2013-07-01 01:04:55 +0000

    Another modification of server.js #5

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use(express.bodyParser());
-app.use(express.logger('dev'));
+//app.use(express.bodyParser());
+//app.use(express.logger('dev'));
 //app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
