2770e962de032c97fb4c7697956807bbc07a0bee
Thananat Jitapunkul <thananat@gmail.com>
2013-07-01 00:44:16 +0000

    Changed server file to obviously working one

--- server.js
+++ server.js
@@ -1,32 +1,12 @@
 var express = require('express');
-var app = express();
 
-console.log("Initialized server at port 3000");
-app.listen(3000);
+var app = express.createServer(express.logger());
 
-
-app.use(express.bodyParser());
-app.use(express.logger('dev'));
-//app.use(express.static(__dirname));
-
-app.get('/', function(req, res) {
-	res.send("Hello world!");
+app.get('/', function(request, response) {
+  response.send('Hello World!\nListening on port' + process.env.PORT);
 });
 
-app.get('/endpoint', function(req, res) {
-	console.log("GET Received by server");
-	res.contentType('text/plain');
-	res.send("GET received...");
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
-
-app.post('/endpoint', function(req, res){
-    console.log("Data received");
-	console.log("Host: " + req.host);
-    console.log('Body: ' + JSON.stringify(req.body));
-
-	console.log("Responding with: " + JSON.stringify(req.body));
-	res.json(req.body);
-
-//	res.send({name: 'Thananat Jitapunkul', age: '25'});
-});
-
