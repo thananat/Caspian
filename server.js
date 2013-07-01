72efef7ee0e04385d07aae9ae4ae9cd1f6206a75
Thananat Jitapunkul <thananat@gmail.com>
2013-07-01 00:50:54 +0000

    Create modified server file with slight modification

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 var express = require('express');
+var app = express.createServer(express.logger());
+//var app = express();
 
-var app = express();
+console.log("Initialized server at port 3000");
+app.listen(3000);
 
-app.get('/', function(request, response) {
-  response.send('Hello World!\nListening on port' + process.env.PORT);
+
+app.use(express.bodyParser());
+app.use(express.logger('dev'));
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res) {
+	res.send("Hello world!");
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.get('/endpoint', function(req, res) {
+	console.log("GET Received by server");
+	res.contentType('text/plain');
+	res.send("GET received...");
 });
+
+app.post('/endpoint', function(req, res){
+    console.log("Data received");
+	console.log("Host: " + req.host);
+    console.log('Body: ' + JSON.stringify(req.body));
+
+	console.log("Responding with: " + JSON.stringify(req.body));
+	res.json(req.body);
+
+//	res.send({name: 'Thananat Jitapunkul', age: '25'});
+});
+
