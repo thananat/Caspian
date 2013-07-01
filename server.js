2e838a4f27b0c69c3511eaedfe6e7ab7d2596da7
Thananat Jitapunkul <thananat@gmail.com>
2013-07-01 00:47:33 +0000

    Server file modified #1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 app.get('/', function(request, response) {
   response.send('Hello World!\nListening on port' + process.env.PORT);
