6dd878b7436248f77971ac3f63ebf3953dd1295c
Thananat Jitapunkul <thananat@gmail.com>
2013-07-01 01:14:08 +0000

    Another modification of server.js #9

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var app = express();
-//var app = express.createServer(express.logger());
+//var app = express();
+var app = express.createServer(express.logger());
 
 //app.use(express.bodyParser());
 app.use(express.logger('dev'));
